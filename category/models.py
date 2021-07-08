36c70b815adf996d83933be22ca20f39eb61ef51
Allan Imire <allanimire@gmail.com>
2021-07-08 16:15:14 +0300

    add a base template

---
+++ category/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=20, unique=True)
+    category_slug = models.CharField(max_length=100, unique=True)
+    category_description = models.TextField(max_length=200, blank=True)
+    category_image = models.ImageField(upload_to='images/categories', blank=True)
+
+    def __str__(self):
+        return self.category_name
